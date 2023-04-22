eb6f04e84206dd35b8f50ac64b296c353d397441
donkey4hot <tractorist666@gmail.com>
2023-04-22 18:37:18 +0300

    init

--- todolist/goals/admin.py
+++ todolist/goals/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from todolist.goals.models import GoalCategory
+from todolist.goals.models import GoalCategory, Goal
 
 
 @admin.register(GoalCategory)
@@ -8,3 +8,10 @@ class GoalCategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created', 'updated')
     search_fields = ('title',)
     list_filter = ('is_deleted',)
+
+
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created', 'updated')
+    search_fields = ('title', 'description')
+    list_filter = ('status', 'priority')
